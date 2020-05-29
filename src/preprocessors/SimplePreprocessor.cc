95e04c8d70a7a540c267e69d0384562f190fed4d
Peter Kessen <p.kessen@kessen-peter.de>
2020-05-30 00:14:01 +0200

    Adapted Simple preprocessor to char interface

--- src/preprocessors/SimplePreprocessor.cc
+++ src/preprocessors/SimplePreprocessor.cc
@@ -23,7 +23,7 @@
 
 void SimplePreprocessor::process(ICairoTTYProtected &ctty, char c)
 {
-    if (Glib::Unicode::iscntrl(c))
+    if (iscntrl(c))
     {
         // Control codes handled here
         switch (c)
